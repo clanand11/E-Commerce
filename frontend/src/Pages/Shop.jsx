cce082fea25e2d3ee8066f56b218bccfcaeb5735
clanand11 <itsjarvis11@gmail.com>
2024-08-08 14:04:53 +0530

    frontend completed

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import Newcollections from '../Components/NewCollections/Newcollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+        <Hero/>
+        <Popular/>
+        <Offers/>
+        <Newcollections/>
+        <NewsLetter/>
+    </div>
   )
 }
 
